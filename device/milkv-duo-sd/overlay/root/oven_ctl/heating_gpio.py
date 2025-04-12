f7d6b4567008de29fa21aa16a3648c97c2a0c3b5
gtxzsxxk <zhaohy22@mails.tsinghua.edu.cn>
2025-04-12 13:18:03 +0800

    oven_ctl: feat: support export gpio heating state

--- device/milkv-duo-sd/overlay/root/oven_ctl/heating_gpio.py
+++ device/milkv-duo-sd/overlay/root/oven_ctl/heating_gpio.py
@@ -2,17 +2,21 @@ from pinpong.board import Board,Pin
 from config import CONFIG_HEATING_GPIO
 
 heating_gpios = []
+heating_state = []
 
 def heating_gpio_init():
     Board("MILKV-DUO").begin()
     for gpio in CONFIG_HEATING_GPIO:
         heating_gpios.append(Pin(gpio, Pin.OUT))
+        heating_state.append(False)
         heating_gpios[len(heating_gpios) - 1].value(0)
 
 
 def heating_enable(idx):
     heating_gpios[idx].value(1)
+    heating_state[idx] = True
 
 
 def heating_disable(idx):
     heating_gpios[idx].value(0)
+    heating_state[idx] = False

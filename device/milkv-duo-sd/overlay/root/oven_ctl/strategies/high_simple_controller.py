f0fe40d28d1cf34dc70e78402d3b5ecf69aff2a1
gtxzsxxk <zhaohy22@mails.tsinghua.edu.cn>
2025-04-12 15:32:47 +0800

    oven_ctl: feat: introduce strategies for high and low temp sensors

---
+++ device/milkv-duo-sd/overlay/root/oven_ctl/strategies/high_simple_controller.py
@@ -0,0 +1,29 @@
+def heating_strategy(temp_high: float, temp_low: float, working_seconds:float, argument: str):
+    args = argument.split(' ')
+    if len(args) != 3:
+        return -1 # 策略执行失败
+    target = float(args[0])
+    heating_time = float(args[1])
+    sensitivity = float(args[2])
+    if temp_high >= target:
+        heating_disable(0)
+        heating_disable(1)
+    else:
+        heating_enable(0)
+        heating_enable(1)
+
+    if working_seconds > heating_time:
+        return 2 # 加热时间满足要求，结束逻辑
+
+    if abs(target - temp_high) <= sensitivity:
+        return 0 # 达到目标温度
+    else:
+        return 1 # 未达到目标温度
+
+
+def heating_description():
+    return {
+        "name": "高温简单比例控制器",
+        "argument": "目标温度 恒温保持时间 敏感度",
+        "trusted": True
+    }

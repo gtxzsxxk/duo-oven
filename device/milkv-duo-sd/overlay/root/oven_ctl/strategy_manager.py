83640e54dcf9990466b1f4dbc0b508a298b53d7f
gtxzsxxk <zhaohy22@mails.tsinghua.edu.cn>
2025-04-12 13:15:40 +0800

    oven_ctl: feat: introduce strategy manager to load and exec strategy python file

--- device/milkv-duo-sd/overlay/root/oven_ctl/strategy_manager.py
+++ device/milkv-duo-sd/overlay/root/oven_ctl/strategy_manager.py
@@ -1,12 +1,60 @@
 import os
 import builtins
+from heating_gpio import heating_enable, heating_disable
 
-selected_strategy = ""
+# 允许执行的内建函数
+safe_builtins = {
+    "float": builtins.float,
+    "len": builtins.len,
+    "abs": builtins.abs,
+    "str": builtins.str,  # 添加 str，避免 str.split() 报错
+    "print": builtins.print,  # 允许打印
+}
 
-def sm_get_all():
-    for f in os.listdir("strategies"):
-        file_path = os.paht.join("strategies", f)
-        if os.path.isfile(os.paht.join("strategies", f)):
-            fp = open(file_path, "r")
-            code = fp.read()
-            
+safe_globals = {
+    "__builtins__": safe_builtins,
+    "heating_enable": heating_enable,
+    "heating_disable": heating_disable,
+}
+
+
+def sm_strategy_load_code(filename):
+    fp = open(os.path.join("strategies", filename), "r")
+    code = fp.read()
+    fp.close()
+    return code
+
+
+def sm_strategy_register(code):
+    exec(code, safe_globals)
+
+
+def sm_strategy_get_desc():
+    return safe_globals["heating_description"]()
+
+
+def sm_strategy_exec(temp_high: float, temp_low: float, working_seconds:float, argument: str):
+    return safe_globals["heating_strategy"](temp_high, temp_low, working_seconds, argument)
+
+
+def sm_strategy_delete(filename):
+    try:
+        os.remove(os.path.join("strategies", filename))
+        return "ok"
+    except:
+        return "failed"
+
+
+def sm_strategies_get_all():
+    all_desc = []
+    try:
+        for f in os.listdir("strategies"):
+            if os.path.isfile(os.path.join("strategies", f)):
+                sm_strategy_register(sm_strategy_load_code(f))
+                desc = sm_strategy_get_desc()
+                desc["filename"] = f
+                all_desc.append(desc)
+    except:
+        return []
+
+    return all_desc
